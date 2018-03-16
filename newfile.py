7e3174d1509967b909cf7df88fa3edf0bf56b23e
h.calero.2017 <hugocdmr@gmail.com>
2018-03-16 16:16:29 +0100

    initial upload of newfile.py

---
+++ newfile.py
@@ -0,0 +1,11 @@
+a = 1
+b = 0
+print("HEllo  from Pycharm 0")
+print("HEllo  from Pycharm 1")
+print("HEllo  from Pycharm 2")
+
+print("HEllo  from Pycharm 3")
+print("HEllo  from Pycharm 4")
+print("HEllo  from Pycharm 5")
+print("HEllo  from Pycharm 6")
+print("HEllo  from Pycharm 7")
